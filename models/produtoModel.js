e473cd7c8067e1688ffb910406be2e7efe7635f8
Nivaldo T <nivaldoj1@outlook.com>
2025-10-13 18:27:46 -0300

    categoria

--- models/produtoModel.js
+++ models/produtoModel.js
@@ -5,8 +5,9 @@ class produtoModel{
     #tipo;
     #nome;
     #preco;
-    #descricao
-    #categoria
+    #descricao;
+    #categoria;
+    #categoria_nome;
 
     set id(valor){this.#id = valor};
     get id(){return this.#id};
@@ -20,15 +21,19 @@ class produtoModel{
     get descricao(){return this.#descricao};
     set categoria(valor){this.#categoria = valor};
     get categoria(){return this.#categoria};
+    set categoria_nome(valor){this.#categoria_nome = valor};
+    get categoria_nome(){return this.#categoria_nome};
 
-    constructor(id,tipo,nome,preco,descricao, categoria){
+    constructor(id,tipo,nome,preco,descricao, categoria, categoria_nome){
         this.#id = id;
         this.#tipo = tipo;
         this.#nome = nome;
         this.#preco = preco;
         this.#descricao = descricao;
         this.#categoria = categoria;
+        this.#categoria_nome = categoria_nome;
     }
+
     async cadastrar(){
         const sql = 'insert into tb_Produto(prod_nome,prod_tipo,prod_preco,prod_descricao, prod_categoria) values(?,?,?,?,?)';
         const valores = [this.#nome, this.#tipo, this.#preco, this.#descricao, this.#categoria];
@@ -39,13 +44,13 @@ class produtoModel{
     }
 
     async listar(){
-        const sql ='select * from tb_Produto';
+        const sql ='select * from tb_Produto p inner join tb_Categoria c on p.prod_categoria = c.categoria_id';
         const banco = new Database();
         const rows = await banco.ExecutaComando(sql);
         
         let lista = [];
         for(let i=0; i<rows.length;i++){
-            lista.push(new produtoModel(rows[i]['prod_id'],rows[i]['prod_tipo'], rows[i]['prod_nome'], rows[i]['prod_preco'], rows[i]['prod_descricao'], rows[i]['prod_categoria']));
+            lista.push(new produtoModel(rows[i]['prod_id'],rows[i]['prod_tipo'], rows[i]['prod_nome'], rows[i]['prod_preco'], rows[i]['prod_descricao'], rows[i]['prod_categoria'], rows[i]['categoria_nome']));
         }
 
         return lista;
@@ -56,8 +61,8 @@ class produtoModel{
         const valores = [id];
         const banco = new Database();
         
-        const result = await banco.ExecutaComando(sql,valores);
-        let prod = new produtoModel(result['0']['prod_id'], result['0']['prod_tipo'], result['0']['prod_nome'], result['0']['prod_preco'], result['0']['prod_descricao'], rows[i]['prod_categoria']);
+        const row = await banco.ExecutaComando(sql,valores);
+        let prod = new produtoModel(row['0']['prod_id'], row['0']['prod_tipo'], row['0']['prod_nome'], row['0']['prod_preco'], row['0']['prod_descricao'], row['0']['prod_categoria']);
         return prod;
     }
 
