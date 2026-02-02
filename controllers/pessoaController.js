a03065e8bb836069ee67d05394205a85d933c3c3
Nivaldo T <nivaldoj1@outlook.com>
2026-02-02 13:27:22 -0300

    Adicionado Autorização com JWT

--- controllers/pessoaController.js
+++ controllers/pessoaController.js
@@ -1,6 +1,7 @@
 const pessoaModel = require('../models/pessoaModel');
 const PFisicaModel = require('../models/pfisicaModel');
 
+const jwt = require('jsonwebtoken');
 class pessoaController{
     async buscarClienteNome(req,res){
         let nome = req.body.nome;
@@ -27,35 +28,50 @@ class pessoaController{
         
         usuario = await usuario.logarEmailSenha(); //busca se existe o usuario com esse email e senha
         
-        //se encontrar, verifica se é funcionario
         if(usuario){
+            let usuarioFunc = new PFisicaModel(usuario.id);
+            usuarioFunc = await usuarioFunc.buscarId()
+            let isFunc; //verifica se é funcionario para Adicionar no token JWT
+            if(usuarioFunc.isFunc)
+                isFunc = true;
+            else
+                isFunc = false;
+
             res.cookie('UsuarioEmail', usuario.email);
             res.cookie('UsuarioSenha', usuario.senha);
             res.cookie('UsuarioNome', usuario.nome);
             ok = true;
             msg = 'Login Realizado com Sucesso!';
+            var token = jwt.sign({
+                id: usuario.id,
+                isFunc: isFunc,
+                exp: Math.floor(Date.now()/1000) + (60*60)
+            }, process.env.segredo);
+            res.cookie('tokenUsuario', token);
         } else {
            
             ok = false; //credencial invalida
             msg = 'Email ou Senha Incorretos!';
             redirecionarPara = null;
         }
-    
         res.send({ok, msg, redirecionarPara});
     }
 
     async buscarPessoaLogin(req,res){
         let ok;
-        let email = req.cookies.UsuarioEmail;
-        let senha = req.cookies.UsuarioSenha;
+        // let pessoa = new PFisicaModel(null,null,null,email,senha); //antes precisava ver no banco de dados sempre
+        // pessoa = await pessoa.logarEmailSenha();
 
-        let pessoa = new PFisicaModel(null,null,null,email,senha);
-        pessoa = await pessoa.logarEmailSenha();
+        let token = req.cookies.tokenUsuario;     //recebe o token do navegador, decodifica e vê se é funcionário
+        if(token){
 
-        if(pessoa && pessoa.isFunc)
-            ok = true;
-        else
-            ok = false;
+            let pessoa = jwt.verify(token, process.env.segredo);
+            
+            if(pessoa && pessoa.isFunc)
+                ok = true;
+            else
+                ok = false;
+        }
         res.send({ok});
     }
 }
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,16 @@
 const PFisicaModel = require('../models/pfisicaModel');
+const jwt = require('jsonwebtoken');
 class authMiddleware{
 
     async verificarUsuarioLogado(req, res, next) {
-        
-        if(req.cookies != undefined && req.cookies.UsuarioEmail != null && req.cookies.UsuarioSenha != null){
-            let func = new PFisicaModel(null,null,null,req.cookies.UsuarioEmail,req.cookies.UsuarioSenha);
-            func = await func.logarEmailSenha();
-            if(func && func.isFunc)
+                                    // && req.cookies != undefined && req.cookies.UsuarioEmail != null && req.cookies.UsuarioSenha != null
+        if(req.cookies.tokenUsuario){
+            // let func = new PFisicaModel(null,null,null,req.cookies.UsuarioEmail,req.cookies.UsuarioSenha);
+            // func = await func.logarEmailSenha();
+            let func = jwt.verify(req.cookies.tokenUsuario, process.env.segredo);
+            //console.log(func)
+            //console.log(jwt.verify(req.cookies.tokenUsuario, process.env.segredo));
+            if(func && func.isFunc && func.exp>(Date.now()/1000))
                 next();
             else
                 res.redirect('/usuario/login');
